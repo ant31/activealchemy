e1b27a11b27e38927dcd77e578ae9bebde14f53b
ant31 <2t.antoine@gmail.com>
2025-04-16 10:04:54 +0200

    lint select test

--- tests/async/test_async_select.py
+++ tests/async/test_async_select.py
@@ -1,7 +1,7 @@
 """
 Unit tests for the async Select class
 """
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock
 
 import pytest
 from sqlalchemy.exc import SQLAlchemyError
